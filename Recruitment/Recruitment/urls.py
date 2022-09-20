aca6d96e0378a3f967719a33f0be7e72a666e0b5
sagar0911 <sanchitsagar0911@gmail.com>
2022-09-20 16:32:43 +0800

    Changing Requirements.txt

--- Recruitment/Recruitment/urls.py
+++ Recruitment/Recruitment/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     
-    path('recruit/', schema_view.with_ui('swagger', cache_timeout=0),
+    path('/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('Applicants/', views.ApplicantList.as_view()),
